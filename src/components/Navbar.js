f89bd24f35159d16ae7fe0c9d4bbe65142015f83
Laura <lauracolof@gmail.com>
2023-04-06 21:29:56 -0300

    changes on /navbar and app.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,23 +32,31 @@ export const NavBar = () => {
   return (
     <Navbar expand="md" variant='dark' className={scrolled ? 'scrolled' : ""}>
       <Container>
-        <Navbar.Brand href="/home">
+        <Navbar.Brand href="/">
           <img src={logo} alt="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className='navbar-toggler-icon'></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
+
+        <Navbar.Collapse className='buttons' id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="home" className={activeLink === 'home' ? 'active nav-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="projects" className={activeLink === 'projects' ? 'active nav-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            <Nav.Link href="about-me" className={activeLink === 'about-me' ? 'active nav-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about-me')}>About me</Nav.Link>
+            <Nav.Link href="home"
+              className={activeLink === 'home' ? 'active nav-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('home')}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="projects"
+              className={activeLink === 'projects' ? 'active nav-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('projects')}>
+              Projects
+            </Nav.Link>
+            <Nav.Link href="about-me"
+              className={activeLink === 'about-me' ? 'active nav-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('about-me')}>
+              About me
+            </Nav.Link>
           </Nav>
           <span className='navbar-text'>
-            <div className='social-icon'>
-              <a href="home">
-                <img src={navIcon1} alt="" />
-              </a>
+            <div className='social-icon' >
+              <a href="https://www.linkedin.com/in/lauracolof/" target="_blank" rel="noreferrer"> LinkedIn </a>
             </div>
 
           </span>
